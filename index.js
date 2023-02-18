e2bd9d4b2ecf1225b6ed43c46aca638f219e3ac9
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-02-18 17:39:48 +0400

    last stop

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 const convertToMill = (h, m, s) => {
   return h * 3600000 + m * 60000 + s * 1000;
 };
+
+// find number of people, on last bus stop
+const findPeopleInBus = (arr) => {
+  let peopleIn = [];
+  let peopleOut = [];
+  arr.forEach((val) => {
+    peopleIn.push(val[0]);
+    peopleOut.push(val[1]);
+  });
+  const first = peopleIn.reduce((acc, curr) => acc + curr, 0);
+  const second = peopleOut.reduce((acc, curr) => acc + curr, 0);
+  return first - second;
+};
